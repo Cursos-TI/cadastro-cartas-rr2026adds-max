a9223e770ad5c77d568f88f17f031eb5c41b79bd
rr2026adds-max <rr2026adds@gmail.com>
2026-04-27 05:43:13 +0000

    Adicionado AeB

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,45 +6,42 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-  char Carta [50];
-  char Pais [50];
-  char Estado [50];
-  char Cidade [50];
-  int Populacao;
-  float Area;
-  float Pib;
-  int Pontosturisticos;
-  
+  char Pais [50] ,EstadoA [50] ,EstadoB [50] ,CidadeA [50] ,CidadeB [50];
+  int A01, B01, PopulacaoA, PopulacaoB, PontosturisticosA, PontosturisticosB;
+  float AreaA, AreaB, PibA, PibB;
+    
   // Área para entrada de dados
-  printf("digite sua carta:\n", Carta);
-  scanf("%s", &Carta);
+  printf("digite qual sera a carta:\n", A01);
+  scanf("%d", &A01);
+
+  printf("");
 
   printf("digite o pais:\n", Pais);
   scanf("%s", &Pais);
 
-  printf("digite o estado:\n", Estado);
-  scanf("%s", &Estado);
+  printf("digite o estado de A ou B:\n", EstadoA, EstadoB);
+  scanf("%s", &EstadoA, EstadoB);
 
-  printf("digite a cidade:\n", Cidade);
-  scanf("%s", &Cidade);
+  printf("digite a cidade de A ou B:\n", CidadeA, CidadeB);
+  scanf("%s", &CidadeA, CidadeB);
 
-  printf("digite a população:\n", Populacao);
-  scanf("%d", &Populacao);
+  printf("digite a população de A ou B:\n", PopulacaoA, PopulacaoB);
+  scanf("%d", &PopulacaoA, PopulacaoB);
 
-  printf(" digite a Área:\n", Area);
-  scanf("%f", &Area);
+  printf(" digite a Área de A ou B:\n", AreaA, AreaB);
+  scanf("%f", &AreaA, AreaB);
 
-  printf("digite o PIB:\n", Pib);
-  scanf("%f", &Pib);
+  printf("digite o PIB de A ou B:\n", PibA, PibB);
+  scanf("%f", &PibA, PibB);
 
-  printf("digite os Pontos turisticos:\n", Pontosturisticos);
-  scanf("%d", &Pontosturisticos);
+  printf("digite os Pontos turisticos de A ou B:\n", PontosturisticosA, PontosturisticosB);
+  scanf("%d", &PontosturisticosA, PontosturisticosB);
 
   // Área para exibição dos dados da cidade
-  printf(" - Carta: %s\n - Pais: %s\n", Carta, Pais);
-  printf(" - Estado: %s\n - Cidade: %s\n", Estado, Cidade);
-  printf(" - População: %d\n - Área: %.2f ", Populacao, Area);
-  printf(" - PIB: %f - Pontos Turisticos: %d\n", Pib, Pontosturisticos);
+  printf(" - A01: %d - B01: %d\n - Pais: %s\n", A01, B01, Pais);
+  printf(" - Estado A: %s - Estado B: %s\n - Cidade A: %s - Cidade B: %s\n", EstadoA, EstadoB, CidadeA, CidadeB);
+  printf(" - População A: %d - População B: %d\n - Área A: %f - Área B: %f\n", PopulacaoA, PopulacaoB, AreaA, AreaB);
+  printf(" - PIB A: %f - PIB B: %f\n - Pontos Turisticos A: %d - Pontos Turisticos B: %d", PibA, PibB, PontosturisticosA, PontosturisticosB);
   
   
 return 0;
